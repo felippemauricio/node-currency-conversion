e713073d6e36dd01feb3a6dd939cf106e1db453d
Felippe Maurício <felippemauriciov@gmail.com>
2019-01-24 22:11:33 -0200

    Add compression on responses

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
+const compression = require('compression');
 const app = require('express')();
-const addRequestId = require('express-request-id')();
+const addRequestId = require('express-request-id');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const { register } = require('./core/helpers');
@@ -12,9 +13,10 @@ const undefinedRouter = (_req, res) => {
 
 
 register(
-  [addRequestId],
-  [morgan('dev')],
+  [addRequestId()],
+  [compression()],
   [helmet()],
+  [morgan('dev')],
   [routers],
   [undefinedRouter],
 )(app);
