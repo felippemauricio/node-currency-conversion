32c4da9651089759a095e319b5985d8731e63b75
Felippe Maurício <felippemauriciov@gmail.com>
2019-01-26 14:36:00 -0200

    Add pm2

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,4 @@ RUN yarn install
 
 EXPOSE 3000
 
-CMD ["node", "server.js"]
+CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -1,27 +1,8 @@
-const cluster = require('cluster');
-const os = require('os');
 const app = require('./src/app');
 const { env } = require('./src/core/helpers');
 
 
-const cpus = os.cpus();
-if (cluster.isMaster) {
-  cpus.forEach(() => {
-    cluster.fork();
-  });
-  cluster.on('listening', (worker) => {
-    console.log(`Cluster ${worker.process.pid} connected`);
-  });
-  cluster.on('disconnect', (worker) => {
-    console.log(`Cluster ${worker.process.pid} disconnected`);
-  });
-  cluster.on('exit', (worker) => {
-    console.log(`Cluster ${worker.process.pid} lost`);
-    cluster.fork();
-  });
-} else {
-  const server = app.listen(env.PORT || 3000, () => {
-    const { port } = server.address();
-    console.log(`CHALLENGE-BRAVO STARTED ON PORT: ${port}!`);
-  });
-}
+const server = app.listen(env.PORT, () => {
+  const { port } = server.address();
+  console.log(`CURRENCY-CONVERSION STARTED ON PORT: ${port}!`); // eslint-disable-line no-console
+});
